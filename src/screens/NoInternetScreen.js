57ebba15ebaa1cbd106bea9aa8062ee84f7869d6
Naveen Nagam <naveennagam6015@gmail.com>
2025-09-02 15:13:55 +0530

    pull for notifications

--- src/screens/NoInternetScreen.js
+++ src/screens/NoInternetScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import { View, Image } from "react-native";
 import CustomText from "../components/CustomText";
 import globalStyles from "../styles/globalStyles";
-import NoInternetImage from "../../assets/icons/NetworkProvider.png"; 
+import NoInternetImage from "../../assets/icons/NetworkProvider.png";
 
 export default function NoInternetScreen() {
   return (
@@ -15,9 +15,14 @@ export default function NoInternetScreen() {
       ]}
     >
       <Image
-           style={{ width: 220, height: 220, resizeMode: "contain" }} 
-           source={NoInternetImage}
-         />
+        style={{ width: 220, height: 220, resizeMode: "contain" }}
+        source={NoInternetImage}
+      />
+      <CustomText
+        style={[globalStyles.f14Bold, globalStyles.primary, globalStyles.mt2]}
+      >
+        No Internet Connection
+      </CustomText>
     </View>
   );
 }
